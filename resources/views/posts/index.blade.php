74b3d344d06845ed129445f5507cf6e6fe7eb4b5
luanaflorencio <lua.m.016@gmail.com>
2022-02-26 22:13:21 -0300

    Adicionando arquivos que faltavam iniciando ajustes no index do post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -15,6 +15,9 @@
                             <thead>
                             <tr>
                                 <th>Título</th>
+                                <th>Conteúdo</th>
+                                <th>Criado por:</th>
+                                <th>Status</th>
                                 <th></th>
                             </tr>
                             </thead>
@@ -22,6 +25,9 @@
                             @foreach($posts as $post)
                                 <tr>
                                     <td>{{ $post->title }}</td>
+                                    <td>{{ $post->body}}</td>
+                                    <td>{{ $post->id }}</td>
+                                    <td>{{ "Não publicado"}}</td>
                                     <td>
                                         <a href="{{ url('posts/edit/' . $post->id) }}" style="border-color: #194765" class="btn btn-light">
                                             <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="17px" viewBox="0 0 24 24" width="24px" fill="#194765"><g><rect fill="none" height="24" width="24"/></g><g><g><g><path d="M3,21l3.75,0L17.81,9.94l-3.75-3.75L3,17.25L3,21z M5,18.08l9.06-9.06l0.92,0.92L5.92,19L5,19L5,18.08z"/></g><g><path d="M18.37,3.29c-0.39-0.39-1.02-0.39-1.41,0l-1.83,1.83l3.75,3.75l1.83-1.83c0.39-0.39,0.39-1.02,0-1.41L18.37,3.29z"/></g></g></g></svg>
